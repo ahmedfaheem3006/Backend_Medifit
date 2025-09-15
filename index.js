fef087a4381f2563a4bf0b351e9def078404609e
ahmedfaheem3006 <ahmedmahmoud30006@gmail.com>
2025-09-15 19:48:33 +0300

    Update backend URL to new Railway domain

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://backed-medifit-production.up.railway.app/auth/google/callback",
+      callbackURL: "https://medifitbackend-production.up.railway.app/auth/google/callback",
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
